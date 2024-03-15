9e2b402a24049687f35c6280d86d23e3a0978682
Robert N. M. Watson <robert.watson@cl.cam.ac.uk>
2024-03-15 12:54:42 +0000

    Basic setup script (to be copied over before cheri-demos can be cloned).

---
+++ 20240328-darpa-cpm/setup.sh
@@ -0,0 +1,8 @@
+# Packages
+pkg64 install gdb-cheri ghidra-cheri llvm qemu
+pkg64c install cheri-desktop cheri-vm-support chericat git tmux
+
+# Git repos
+git clone https://github.com/CTSRD-CHERI/cheri-demo
+git clone https://github.com/CTSRD-CHERI/cheri-exercises
+git clone https://github.com/CTSRD-CHERI/cheribsd-tutorial
