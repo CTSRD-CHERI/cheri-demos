6dd0836f0b762e493911b827d9da2eba61fb24d0
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2024-07-03 23:54:51 +0100

    20240704-opendays: add a fixed version

---
+++ 20240704-opendays/login-fixed.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "login.h"
+
+static void
+do_login(void)
+{
+	char *argv[2] = {
+		"./shell",
+		NULL
+	};
+
+	execve(argv[0], (char **)&argv, NULL);
+}
+
+int
+main(void)
+{
+	char result;
+	char name[] = USER;
+	char input[sizeof(PASSWORD)];
+	int index;
+	char c;
+
+	result = 0;
+	for (;;) {
+		printf("Hello, %s!\n", name);
+		printf("password: ");
+
+		index = 0;
+		while ((c = getchar()) != '\n') {
+			if (index > sizeof(input) - 1)
+				continue;
+			input[index++] = c;
+		}
+		if (index <= sizeof(input) - 1)
+			input[index] = '\0';
+
+		if (strcmp(input, PASSWORD) == 0)
+			result = 1;
+
+		if (result != 0) {
+			printf("\nSuccess! Type 'exit' to log out.\n\n");
+			do_login();
+		} else {
+			printf("Wrong password. Try again.\n\n");
+		}
+	}
+
+	return (0);
+}
