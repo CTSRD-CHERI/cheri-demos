79a35c0913de63e7d078277cca2b8eb6a15858c8
Alfredo Mazzinghi <alfredo.mazzinghi@cl.cam.ac.uk>
2025-02-06 12:37:59 +0000

    dsbd-showcase: Add template for page serving chromium CVEs.
    This will link one or more CVE pages for the demo.

--- 20250211-dsbd-demo/nginx/setup.sh
+++ 20250211-dsbd-demo/nginx/setup.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-pkg64c install nginx
-ln -s /usr/local/etc/nginx-cheri /usr/local/etc/nginx
+pkg64c install -y nginx
+if [ ! -e /usr/local/etc/nginx ]; then
+    ln -s /usr/local/etc/nginx-cheri /usr/local/etc/nginx
+fi
+if [ -e /usr/local/www/dsbd-demo ]; then
+    rm -r /usr/local/www/dsbd-demo
+fi
 cp nginx.conf /usr/local/etc/nginx-cheri/nginx.conf
 wget -r -k -p --no-parent https://www.cl.cam.ac.uk/research/security/ctsrd/cheri/
 cp -r www.cl.cam.ac.uk /usr/local/www/dsbd-demo
 rm www.cl.cam.ac.uk
+cp -r exploits-demo /usr/local/www/exploits
 
 sysrc nginx_enable=YES
 service nginx start
