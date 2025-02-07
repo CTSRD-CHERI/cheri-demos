279a28cf62c2a987e0b68056780263aadae13238
Graeme Jenkinson <graeme@capabilitieslimited.co.uk>
2025-02-07 16:05:57 +0000

    Initial list of runtime dependencies.

--- 20250211-dsbd-demo/chromium/setup.sh
+++ 20250211-dsbd-demo/chromium/setup.sh
@@ -11,22 +11,38 @@ else
     real_user=$(whoami)
 fi
 
+echo "DSbD Showcase 2025-02-11 Chromium Demo"
+
 echo "Checking Chromium runtime dependencies"
 
-CHROMIUM_RUNTIME_DEPS= at-spi2-core \
+# Missing:
+# dav1d - patches ready but no official port
+# snappy - no patches ready for this
+CHROMIUM_RUNTIME_DEPS= \
+    at-spi2-core \
     boringssl \
     cairo \
+    flac \
     gtk3 \
+    fontconfig \
+    freetype \
     harfbuzz \
+    icu \
+    libxml2 \
+    libxlst \
     mesa-libs \
     mesa-dria \
+    noto-basic \
     nspr \
     nss \
+    openh264 \
+    opus \
+    png \
     pango
 
 if $(pkg64c check -d $CHROMIUM_RUNTIME_DEPS); then
     echo "All Chromiumn runtime dependencies found"
 else
     echo "Installing Chromium runtime dependencies"
-    pkg64c instal $(CHROMIUM_RUNTIME_DEPS
+    pkg64c install $(CHROMIUM_RUNTIME_DEPS)
 fi
