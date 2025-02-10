2945fd155707aab4ac71bb484e4d7b9cb2179558
Alfredo Mazzinghi <alfredo.mazzinghi@cl.cam.ac.uk>
2025-02-10 20:02:12 +0000

    dsdb-showcase: Update nginx setup script to avoid regenerating files on update.

--- 20250211-dsbd-demo/nginx/setup.sh
+++ 20250211-dsbd-demo/nginx/setup.sh
@@ -4,13 +4,11 @@ pkg64c install -y nginx
 if [ ! -e /usr/local/etc/nginx ]; then
     ln -s /usr/local/etc/nginx-cheri /usr/local/etc/nginx
 fi
-if [ -e /usr/local/www/dsbd-demo ]; then
-    rm -r /usr/local/www/dsbd-demo
-fi
 cp nginx.conf /usr/local/etc/nginx-cheri/nginx.conf
-wget -r -k -p --no-parent https://www.cl.cam.ac.uk/research/security/ctsrd/cheri/
-cp -r www.cl.cam.ac.uk /usr/local/www/dsbd-demo
-rm -r www.cl.cam.ac.uk
+if [ ! -d /usr/local/www/dsbd-demo ]; then
+    wget -r -k -p --no-parent https://www.cl.cam.ac.uk/research/security/ctsrd/cheri/
+    mv www.cl.cam.ac.uk /usr/local/www/dsbd-demo
+fi
 cp -r exploits-demo /usr/local/www/exploits
 
 sysrc nginx_enable=YES
