49494b19892ea2070c8088fdf8b999b5b741aa2e
Hesham Almatary <heshamelmatary@users.noreply.github.com>
2025-02-11 10:52:57 +0000

    Update run-sel4-cheri.sh
    Change -G port number for sel4cheri to be able to run both aarch64 and cheri versions at the same time

--- 20250211-dsbd-demo/sel4/run-sel4-cheri.sh
+++ 20250211-dsbd-demo/sel4/run-sel4-cheri.sh
@@ -3,7 +3,7 @@
 sudo bhyve -c 1 -m 1g \
      -s 0,hostbridge \
      -s 2,virtio-blk,${1} \
-     -G 1234 -S \
+     -G 1235 -S \
      -o bootrom=/usr/local/share/u-boot/u-boot-bhyve-arm64/u-boot.bin \
      -o console=stdio \
      sel4cheri
